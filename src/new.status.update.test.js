f8164026434dec0621df703e0fdaa11643014ebd
Brian Smith <briaguya@gmail.com>
2017-07-13 19:54:22 -0500

    refacccctooooorrrrr

--- src/new.status.update.test.js
+++ src/new.status.update.test.js
@@ -20,106 +20,41 @@ async function getToken(email, password) {
   return response.authenticate.jwtToken;
 }
 
-function shouldWork(username, password, status, crossing, notes, reason, duration) {
+function shouldWork(username, password, status, crossing, notes, reason, duration, extra_description) {
   var originalStatusId;
   var newStatusId;
   var lokka;
 
-  beforeAll(async (done) => {
-    getToken(username, password).then((token) => {
-      const headers = {
-        'Authorization': 'Bearer '+ token
-      };
-      lokka = new Lokka({transport: new HttpTransport('http://localhost:5000/graphql', {headers})});
-      done();
+  describe('as ' + username + ' ' + (extra_description || ''), () => {
+    beforeAll(async (done) => {
+      getToken(username, password).then((token) => {
+        const headers = {
+          'Authorization': 'Bearer '+ token
+        };
+        lokka = new Lokka({transport: new HttpTransport('http://localhost:5000/graphql', {headers})});
+        done();
+      });
     });
-  });
 
-  it('before updating it should get the current status', async () => {
-    const response = await lokka.send(`
-      query($crossing:Int!) {
-        crossingById(id:$crossing) {
-          latestStatus {
-            id
+    it('before updating it should get the current status', async () => {
+      const response = await lokka.send(`
+        query($crossing:Int!) {
+          crossingById(id:$crossing) {
+            latestStatus {
+              id
+            }
           }
         }
-      }
-    `,
-    {
-      crossing: crossing
+      `,
+      {
+        crossing: crossing
+      });
+
+      originalStatusId = response.crossingById.latestStatus.id;
+      expect(response).not.toBeNull();
     });
 
-    originalStatusId = response.crossingById.latestStatus.id;
-    expect(response).not.toBeNull();
-  });
-
-  it('should update the status', async () => {
-    const response = await lokka.send(`
-      mutation($status:Int,$crossing:Int,$notes:String,$reason:Int,$duration:Int) {
-        newStatusUpdate(input: {
-          statusId: $status,
-          crossingId: $crossing,
-          notes: $notes,
-          statusReasonId: $reason,
-          statusDurationId: $duration
-        }) {
-          statusUpdate {
-            id
-            notes
-          }
-        }
-      }
-    `,
-    {
-      status: status,
-      crossing: crossing,
-      notes: notes,
-      reason: reason,
-      duration: duration
-    });
-
-    newStatusId = response.newStatusUpdate.statusUpdate.id;
-    expect(response.newStatusUpdate.statusUpdate.notes).toMatchSnapshot();
-  });
-
-  it('the new status should show up in the DB', async () => {
-    const response = await lokka.send(`
-      query($crossing:Int!) {
-        crossingById(id:$crossing) {
-          latestStatus {
-            id
-            notes
-          }
-        }
-      }
-    `,
-    {
-      crossing: crossing
-    });
-
-    expect(response.crossingById.latestStatus.id).not.toEqual(originalStatusId);
-    expect(response.crossingById.latestStatus.id).toEqual(newStatusId);
-    expect(response.crossingById.latestStatus.notes).toMatchSnapshot();
-  });
-}
-
-function shouldFail(username, password, status, crossing, notes, reason, duration) {
-  var originalStatusId;
-  var newStatusId;
-  var lokka;
-
-  beforeAll(async (done) => {
-    getToken(username, password).then((token) => {
-      const headers = {
-        'Authorization': 'Bearer '+ token
-      };
-      lokka = new Lokka({transport: new HttpTransport('http://localhost:5000/graphql', {headers})});
-      done();
-    });
-  });
-
-  it('should fail to update the status', async () => {
-    try {
+    it('should update the status', async () => {
       const response = await lokka.send(`
         mutation($status:Int,$crossing:Int,$notes:String,$reason:Int,$duration:Int) {
           newStatusUpdate(input: {
@@ -143,9 +78,78 @@ function shouldFail(username, password, status, crossing, notes, reason, duratio
         reason: reason,
         duration: duration
       });
-    } catch(e) {
-      expect(e).toMatchSnapshot();
-    }
+
+      newStatusId = response.newStatusUpdate.statusUpdate.id;
+      expect(response.newStatusUpdate.statusUpdate.notes).toMatchSnapshot();
+    });
+
+    it('the new status should show up in the DB', async () => {
+      const response = await lokka.send(`
+        query($crossing:Int!) {
+          crossingById(id:$crossing) {
+            latestStatus {
+              id
+              notes
+            }
+          }
+        }
+      `,
+      {
+        crossing: crossing
+      });
+
+      expect(response.crossingById.latestStatus.id).not.toEqual(originalStatusId);
+      expect(response.crossingById.latestStatus.id).toEqual(newStatusId);
+      expect(response.crossingById.latestStatus.notes).toMatchSnapshot();
+    });
+  });
+}
+
+function shouldFail(username, password, status, crossing, notes, reason, duration, extra_description) {
+  var originalStatusId;
+  var newStatusId;
+  var lokka;
+
+  describe('as ' + username + ' ' + (extra_description || ''), () => {
+    beforeAll(async (done) => {
+      getToken(username, password).then((token) => {
+        const headers = {
+          'Authorization': 'Bearer '+ token
+        };
+        lokka = new Lokka({transport: new HttpTransport('http://localhost:5000/graphql', {headers})});
+        done();
+      });
+    });
+
+    it('should fail to update the status', async () => {
+      try {
+        const response = await lokka.send(`
+          mutation($status:Int,$crossing:Int,$notes:String,$reason:Int,$duration:Int) {
+            newStatusUpdate(input: {
+              statusId: $status,
+              crossingId: $crossing,
+              notes: $notes,
+              statusReasonId: $reason,
+              statusDurationId: $duration
+            }) {
+              statusUpdate {
+                id
+                notes
+              }
+            }
+          }
+        `,
+        {
+          status: status,
+          crossing: crossing,
+          notes: notes,
+          reason: reason,
+          duration: duration
+        });
+      } catch(e) {
+        expect(e).toMatchSnapshot();
+      }
+    });
   });
 }
 
@@ -157,25 +161,11 @@ const everyPassword = 'texasfloods';
 describe('When updating the status of a crossing', () => {
   describe('To OPEN', () => {
     describe('with no REASON or DURATION', () => {
-      describe('as a super admin', () => {
-        shouldWork(superAdminEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION');
-      });
-      describe('as a community admin', () => {
-        describe('In the same community as the crossing', () => {
-          shouldWork(communityAdminEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION');
-        });
-        describe('In another community without the crossing', () => {
-          shouldFail(communityAdminEmail, everyPassword, 1, 7, 'OPEN with no REASON or DURATION');
-        });
-      });
-      describe('as a community editor', () => {
-        describe('In the same community as the crossing', () => {
-          shouldWork(communityEditorEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION');
-        });
-        describe('In another community without the crossing', () => {
-          shouldFail(communityEditorEmail, everyPassword, 1, 7, 'OPEN with no REASON or DURATION');
-        });
-      });
+      shouldWork(superAdminEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION');
+      shouldWork(communityAdminEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION',null,null,'In the same community as the crossing'); 
+      shouldFail(communityAdminEmail, everyPassword, 1, 7, 'OPEN with no REASON or DURATION',null,null,'In another community without the crossing');
+      shouldWork(communityEditorEmail, everyPassword, 1, 3, 'OPEN with no REASON or DURATION',null,null,'In the same community as the crossing'); 
+      shouldFail(communityEditorEmail, everyPassword, 1, 7, 'OPEN with no REASON or DURATION',null,null,'In another community without the crossing');
     });
 
     describe('with REASON', () => {
