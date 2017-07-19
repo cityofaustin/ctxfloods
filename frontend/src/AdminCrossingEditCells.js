f235a34eb25d390643a6a6355c887612e5a9adaf
Brian Smith <briaguya@gmail.com>
2017-07-19 14:19:13 -0500

    added a bunch of broken component stuff

---
+++ frontend/src/AdminCrossingEditCells.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class AdminCrossingEditCells extends React.Component {
+
+  render () {
+    return (
+      <div className='pa3 bg-black-05 ma3'>
+        <p>Created</p>
+        <p>User</p>
+        <p>Status</p>
+      </div>
+    );
+  }
+}
+
+export default AdminCrossingEditCells;
---
+++ frontend/src/AdminCrossingListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class AdminCrossingListItem extends React.Component {
+
+  render () {
+    return (
+      <div className='pa3 bg-black-05 ma3'>
+        <AdminCrossingDescriptionCell/>
+      </div>
+    );
+  }
+}
+
+export default AdminCrossingListItem;
