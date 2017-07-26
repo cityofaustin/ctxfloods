dda6479ffaf8761f5ed0d1760282721a842cac9a
Brian Smith <briaguya@gmail.com>
2017-07-26 15:50:33 -0500

    frontend local test

--- frontend/stories/adminCrossingList.js
+++ frontend/stories/adminCrossingList.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import AdminCrossingList from '../src/AdminCrossingList';
+
 
 storiesOf('Admin Crossing List', module)
   .add('with text', () => (
     <AdminCrossingList/>
   ));
+
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withApolloProvider } from 'storybook-addon-apollo-graphql';
+import { gql, graphql } from 'react-apollo';
+import AdminCrossingList from '../src/AdminCrossingList';
+
+const schema = `
+    type Query {
+        random: Int!
+    }
+`;
+
+const root = {
+  random: () => Math.floor(Math.random() * 10),
+};
+
+
+storiesOf('Random Number', module)
+  .addDecorator(withApolloProvider({ schema, root }))
+  .add('A random number query', () => <ComponentWithGraphql />);
