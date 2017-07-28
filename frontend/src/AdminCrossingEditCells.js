742b24b867db28c5938ff6b9098c6604b29151f6
Brian Smith <briaguya@gmail.com>
2017-07-28 14:26:41 -0500

    radio

--- frontend/src/AdminCrossingEditCells.js
+++ frontend/src/AdminCrossingEditCells.js
@@ -5,9 +5,25 @@ class AdminCrossingEditCells extends React.Component {
   render () {
     return (
       <div className='pa3 bg-black-05 ma3'>
-        <p>Created</p>
-        <p>User</p>
-        <p>Status</p>
+        <form>
+          <div>
+            <input type="radio" id="statusChoiceOpen"
+             name="status" value="open"/>
+            <label for="statusChoiceOpen">Open</label>
+
+            <input type="radio" id="statusChoiceClosed"
+             name="status" value="closed"/>
+            <label for="statusChoiceClosed">Closed</label>
+
+            <input type="radio" id="statusChoiceCaution"
+             name="status" value="caution"/>
+            <label for="statusChoiceCaution">Caution</label>
+
+            <input type="radio" id="statusChoiceLongTermClosure"
+             name="status" value="longTermClosure"/>
+            <label for="statusChoiceLongTermClosure">Long Term Closure</label>
+          </div>
+        </form>
       </div>
     );
   }
--- frontend/src/AdminCrossingListItem.js
+++ frontend/src/AdminCrossingListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AdminCrossingDescriptionCell from './AdminCrossingDescriptionCell';
+import AdminCrossingEditCells from './AdminCrossingEditCells';
 
 class AdminCrossingListItem extends React.Component {
 
@@ -7,6 +8,7 @@ class AdminCrossingListItem extends React.Component {
     return (
       <div className='pa3 bg-black-05 ma3'>
         <AdminCrossingDescriptionCell crossing={this.props.crossing}/>
+        <AdminCrossingEditCells />
       </div>
     );
   }
