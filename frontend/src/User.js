971bd2cb351c2cc1239556c2704fa274f9fc7bea
Mateo Clarke <code@mateoclarke.com>
2017-08-10 18:35:33 -0500

    Add role and community name to frontend

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -3,14 +3,19 @@ import React from 'react'
 class User extends React.Component {
 
   render () {
+    const { firstName, lastName, id, role, communityByCommunityId } = this.props.user;
+    const communityName = communityByCommunityId.name;
+
     return (
       <div className='pa3 bg-black-05 ma3'>
         <div className='pt3'>
-          {this.props.user.firstName} {this.props.user.lastName}&nbsp;
+          {firstName} {lastName}&nbsp;
         </div>
         <div className='pt3'>
-          {this.props.user.id}&nbsp;
+          {id}&nbsp;
         </div>
+        <p>{role}</p>
+        <p>{communityName}</p>
       </div>
     );
   }
