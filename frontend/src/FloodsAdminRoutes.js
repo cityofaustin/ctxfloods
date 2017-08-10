752395cc1ab4eab83af808f2145632e043868bd8
Mateo Clarke <code@mateoclarke.com>
2017-08-10 18:59:11 -0500

    use mange users file

--- frontend/src/FloodsAdminRoutes.js
+++ frontend/src/FloodsAdminRoutes.js
@@ -4,9 +4,9 @@ import Login from './Login';
 import Public from './Public';
 import PrivateRoute from './PrivateRoute';
 import Header from './Header';
+import ManageUsers from './ManageUsers';
 import CreateUser from './CreateUser';
 import AdminCrossingList from './AdminCrossingList';
-import UserList from './UserList';
 import NewStatusUpdate from './NewStatusUpdate';
 import auth from './services/gqlAuth';
 
@@ -20,10 +20,16 @@ class FloodsAdminRoutes extends Component {
           <Route path="/public" component={Public}/>
           <Route path="/crossings" component={AdminCrossingList}/>
           <Route path="/login" component={Login}/>
-          <PrivateRoute path="/dashboard/users" component={UserList} authenticated={auth.isAuthenticated()} authorized={auth.roleAuthorized(['floods_community_admin', 'floods_super_admin'])} />
+          <PrivateRoute path="/dashboard/users" component={ManageUsers}
+            authenticated={auth.isAuthenticated()}
+            authorized={auth.roleAuthorized(['floods_community_admin', 'floods_super_admin'])}
+          />
           <PrivateRoute path="/protected" component={Protected} authenticated={auth.isAuthenticated()}/>
           <PrivateRoute path="/updatestatus" component={NewStatusUpdate} authenticated={auth.isAuthenticated()}/>
-          <PrivateRoute path="/createuser" component={CreateUser} authenticated={auth.isAuthenticated()}/>
+          <PrivateRoute path="/createuser" component={CreateUser}
+            authenticated={auth.isAuthenticated()}
+            authorized={auth.roleAuthorized(['floods_community_admin', 'floods_super_admin'])}
+          />
         </div>
     );
   }
