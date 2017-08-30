5e2b0814293dd3fe4b20d90e529b8ec49caa1a41
Brian Smith <briaguya@gmail.com>
2017-08-29 19:43:57 -0500

    🌂

--- frontend/src/ManageUsersPage/UserList.js
+++ frontend/src/ManageUsersPage/UserList.js
@@ -1,51 +1,102 @@
 import React from 'react';
-import User from './User';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import Table from './Table';
+
+const manageUsersHeaders = [{
+  title: '',
+  type: 'checkbox_select',
+}, {
+  title: 'Name',
+  isFilterale: true,
+  isSortable: true,
+}, {
+  title: 'Role',
+  isFilterale: true,
+  isSortable: true,
+}, {
+  title: 'Community',
+  isFilterale: true,
+  isSortable: true,
+}, {
+  title: 'Last active',
+  isFilterale: true,
+  isSortable: true,
+}];
 
 class UserList extends React.Component {
+  parseRole(role) {
+    const roleArray = role.split("_");
+    roleArray.splice(0, 1)
+
+    return roleArray.map((word) => {
+      return (word.charAt(0).toUpperCase() + word.substr(1));
+    }).join(' ');
+  }
 
   render () {
-    if (this.props.data.loading) {
+    if (!this.props.data || this.props.data.loading) {
       return (<div>Loading</div>)
     }
 
-    if (this.props.data.allUsers == null) {
+    const { communityUsers, allUsers } = this.props.data;
+    if ((communityUsers == null) && (allUsers == null)) {
       // TODO: add error logging
       return (<div>Error Loading Users</div>);
     }
 
+    const users = this.props.currentUser.role === "floods_super_admin"
+      ? allUsers
+      : communityUsers;
+
+    const userData = users.nodes.map((user) => {
+    	return [
+        { isLinked: true, link: `/user/${user.id}`, content: `${user.firstName} ${user.lastName}` },
+        this.parseRole(user.role),
+        user.communityByCommunityId.name,
+        '',
+      ]
+    });
+
     return (
-      <div className='w-100 flex justify-center'>
-        <div className='w-100' style={{ maxWidth: 400 }}>
-          {this.props.data.allUsers.nodes.map((user) =>
-            <User
-              key={user.id}
-              user={user}
-              refresh={() => this.props.data.refetch()}
-            />
-          )}
-        </div>
-      </div>
+      <Table data={userData} headers={manageUsersHeaders} checkboxColumn={true} />
     );
   }
 
 }
 
-const allUsers = gql`
-  {
-    allUsers {
+
+const getUsers = gql`
+  fragment UserData on User {
+    id
+    firstName
+    lastName
+    role
+    communityByCommunityId {
+      id
+      name
+    }
+  }
+  query allUsers($superUser: Boolean!, $communityId: Int!) {
+    allUsers @include(if: $superUser) {
       nodes {
-        id
-        firstName
-        lastName
-        role
-        communityByCommunityId {
-          name
-        }
+        ...UserData
+      }
+    }
+    communityUsers: allUsers(condition: {communityId: $communityId}) @skip(if: $superUser) {
+      nodes {
+        ...UserData
       }
     }
   }
 `;
 
-export default graphql(allUsers)(UserList);
+export default graphql(getUsers, {
+  skip: (ownProps) => !ownProps.currentUser,
+  options: (ownProps) => ({
+    variables: {
+      communityId: ownProps.currentUser.communityId,
+      superUser: ownProps.currentUser.role === "floods_super_admin"
+    }
+  }),
+})(UserList);
