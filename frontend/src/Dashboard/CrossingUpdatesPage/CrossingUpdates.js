ace1389bd163df82c6f3c0153b8902fab021d7ef
Brian Smith <briaguya@gmail.com>
2017-08-31 03:31:28 -0500

    move tab class logic into function

--- frontend/src/Dashboard/CrossingUpdatesPage/CrossingUpdates.js
+++ frontend/src/Dashboard/CrossingUpdatesPage/CrossingUpdates.js
@@ -11,18 +11,21 @@ class CrossingUpdates extends Component {
     this.setState({activeTab: tab});
   }
 
+  getTabClass(tab) {
+    const baseClassName = "CrossingUpdates__tab--";
+    const classModifier = this.state.activeTab === tab ? 'active' : 'inactive';
+
+    return baseClassName + classModifier;
+  }
+
   render() {
     return (
       <div className="CrossingUpdates">
         <div className="flexcontainer">
           <div className="CrossingUpdates__tab--header">SHOW: </div>
-          <div className={this.state.activeTab === 'list' ? 'CrossingUpdates__tab--active' : 'CrossingUpdates__tab--inactive'}
-               onClick={this.changeTab.bind(this,'list')}
-          >LIST</div>
+          <div className={this.getTabClass('list')} onClick={this.changeTab.bind(this,'list')}>LIST</div>
           <div className="CrossingUpdates__tab--header"> | </div>
-          <div className={this.state.activeTab === 'map' ? 'CrossingUpdates__tab--active' : 'CrossingUpdates__tab--inactive'}
-               onClick={this.changeTab.bind(this,'map')} 
-          >MAP</div>
+          <div className={this.getTabClass('map')} onClick={this.changeTab.bind(this,'map')}>MAP</div>
         </div>
         {this.state.activeTab === 'map' ? 
           <div className="CrossingUpdates__map-container">
@@ -34,8 +37,6 @@ class CrossingUpdates extends Component {
       </div>
     );
   }
-
 }
-
 
 export default CrossingUpdates;
