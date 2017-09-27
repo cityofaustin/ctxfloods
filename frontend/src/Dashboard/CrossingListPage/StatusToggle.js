dc0569bd103660460af12291441401bb8e3945e3
Brian Smith <briaguya@gmail.com>
2017-09-27 02:08:08 -0500

    added hover styles

--- frontend/src/Dashboard/CrossingListPage/StatusToggle.js
+++ frontend/src/Dashboard/CrossingListPage/StatusToggle.js
@@ -30,24 +30,48 @@ class StatusToggle extends React.Component {
     this.setState({ selectedStatus: status });
   }
 
+  statusHovered(status) {
+    this.setState({ hoveredStatus: status });
+  }
+
+  clearHover() {
+    this.setState({ hoveredStatus: null });
+  }
+
   render () {
     return (
       <div className="StatusToggleContainer">
         <div 
-          className={"StatusToggle__open" + (this.state.selectedStatus === STATUS_OPEN ? "--selected" : "")}
-          onClick={this.statusClicked.bind(this, STATUS_OPEN)}>
+          className={"StatusToggle__open" + 
+                    (this.state.selectedStatus === STATUS_OPEN ? "--selected" : "") +
+                    (this.state.hoveredStatus === STATUS_OPEN ? " hover" : "")}
+          onClick={this.statusClicked.bind(this, STATUS_OPEN)}
+          onMouseOver={this.statusHovered.bind(this, STATUS_OPEN)}
+          onMouseLeave={this.clearHover.bind(this)}>
           <FontAwesome name='circle-o' />
         </div>
-        <div className={"StatusToggle__caution" + (this.state.selectedStatus === STATUS_CAUTION ? "--selected" : "")}
-          onClick={this.statusClicked.bind(this, STATUS_CAUTION)}>
+        <div className={"StatusToggle__caution" + 
+                       (this.state.selectedStatus === STATUS_CAUTION ? "--selected" : "") + 
+                       (this.state.hoveredStatus === STATUS_CAUTION ? " hover" : "")}
+          onClick={this.statusClicked.bind(this, STATUS_CAUTION)}
+          onMouseOver={this.statusHovered.bind(this, STATUS_CAUTION)}
+          onMouseLeave={this.clearHover.bind(this)}>
           <FontAwesome name='exclamation-triangle' />
         </div>
-        <div className={"StatusToggle__closed" + (this.state.selectedStatus === STATUS_CLOSED ? "--selected" : "")}
-          onClick={this.statusClicked.bind(this, STATUS_CLOSED)}>
+        <div className={"StatusToggle__closed" + 
+                       (this.state.selectedStatus === STATUS_CLOSED ? "--selected" : "") +
+                       (this.state.hoveredStatus === STATUS_CLOSED ? " hover" : "")}
+          onClick={this.statusClicked.bind(this, STATUS_CLOSED)}
+          onMouseOver={this.statusHovered.bind(this, STATUS_CLOSED)}
+          onMouseLeave={this.clearHover.bind(this)}>
           <FontAwesome name='times' />
         </div>
-        <div className={"StatusToggle__longterm" + (this.state.selectedStatus === STATUS_LONGTERM ? "--selected" : "")}
-          onClick={this.statusClicked.bind(this, STATUS_LONGTERM)}>
+        <div className={"StatusToggle__longterm" + 
+                       (this.state.selectedStatus === STATUS_LONGTERM ? "--selected" : "") +
+                       (this.state.hoveredStatus === STATUS_LONGTERM ? " hover" : "")}
+          onClick={this.statusClicked.bind(this, STATUS_LONGTERM)}
+          onMouseOver={this.statusHovered.bind(this, STATUS_LONGTERM)}
+          onMouseLeave={this.clearHover.bind(this)}>
           <FontAwesome name='times' style={{ textDecoration: 'underline' }} />
         </div>
       </div>
