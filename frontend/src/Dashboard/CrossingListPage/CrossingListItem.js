89743652336bbf44a152a983e3a8699c92ebb427
Brian Smith <briaguya@gmail.com>
2017-09-27 23:53:55 -0500

    get sassy

--- frontend/src/Dashboard/CrossingListPage/CrossingListItem.js
+++ frontend/src/Dashboard/CrossingListPage/CrossingListItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
+import './CrossingListItem.css';
 
 class CrossingListItem extends React.Component {
 
   render () {
     return (
-
-      <div>
+      <div style={{width: "1280px"}}>
         <img src={require('./example.png')} style={{"height":"348px"}} />
-        CROSSING LIST ITEM
-        <FontAwesome name='circle-o' />
-        {this.props.status}
-        {this.props.dirty}
+          <div className="CrossingListItemContainer">
+            <div className="CrossingName">Spurlock Valley</div>
+            <div className="flexitem">LOCATION</div>
+            <div className="flexitem">DATETIME</div>
+          </div>
+          <div className="CrossingListItemContainer">
+            <div className="flexitem">STATUS</div>
+            <div className="flexitem">REASON</div>
+            <div className="flexitem">NOTES</div>
+          </div>
       </div>
     );
   }
