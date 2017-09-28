435e14d1bd9dbed2dfe0ccc370bc155b60032d26
Brian Smith <briaguya@gmail.com>
2017-09-27 22:50:00 -0500

    add example image

--- frontend/src/Dashboard/CrossingListPage/CrossingListItem.js
+++ frontend/src/Dashboard/CrossingListPage/CrossingListItem.js
@@ -5,7 +5,9 @@ class CrossingListItem extends React.Component {
 
   render () {
     return (
+
       <div>
+        <img src={require('./example.png')} style={{"height":"348px"}} />
         CROSSING LIST ITEM
         <FontAwesome name='circle-o' />
         {this.props.status}
