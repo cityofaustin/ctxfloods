482fb01674710a2da4da59ba5e57388c1b406d21
Brian Smith <briaguya@gmail.com>
2017-09-28 02:12:03 -0500

    add location control story

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -2,8 +2,9 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories/userList.js');
-  require('../src/stories/statusToggle.js');
   require('../src/stories/crossingListItem.js');
+  require('../src/stories/statusToggle.js');
+  require('../src/stories/crossingListItemLocation.js');
 }
 
 configure(loadStories, module);
