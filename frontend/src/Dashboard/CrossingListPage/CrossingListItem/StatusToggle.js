183af0cd06cc9ece4fa9e8fef01ed96e5eeb437d
Brian Smith <briaguya@gmail.com>
2017-10-05 15:35:24 -0500

    switch status toggle away from strings

--- frontend/src/Dashboard/CrossingListPage/CrossingListItem/StatusToggle.js
+++ frontend/src/Dashboard/CrossingListPage/CrossingListItem/StatusToggle.js
@@ -1,41 +1,23 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
 import './StatusToggle.css';
-
-const STATUS_OPEN = Symbol('open');
-const STATUS_CAUTION = Symbol("caution");
-const STATUS_CLOSED = Symbol("closed");
-const STATUS_LONGTERM = Symbol("longterm");
+import * as statusConstants from './StatusConstants';
 
 class StatusToggle extends React.Component {
   constructor(props) {
     super(props);
-
-    switch (props.status) {
-      case 'Open':
-        this.state = { selectedStatus: STATUS_OPEN }
-        break;
-      case 'Caution':
-        this.state = { selectedStatus: STATUS_CAUTION }
-        break;
-      case 'Closed':
-        this.state = { selectedStatus: STATUS_CLOSED }
-        break;
-      case 'Long Term Closure':
-        this.state = { selectedStatus: STATUS_LONGTERM }
-        break;
-    }
+    this.state = { selectedStatus: props.status };
   }
 
-  openClicked = () => { this.setState({ selectedStatus: STATUS_OPEN }) };
-  cautionClicked = () => { this.setState({ selectedStatus: STATUS_CAUTION }) };
-  closedClicked = () => { this.setState({ selectedStatus: STATUS_CLOSED }) };
-  longtermClicked = () => { this.setState({ selectedStatus: STATUS_LONGTERM }) };
+  openClicked = () => { this.setState({ selectedStatus: statusConstants.OPEN }) };
+  cautionClicked = () => { this.setState({ selectedStatus: statusConstants.CAUTION }) };
+  closedClicked = () => { this.setState({ selectedStatus: statusConstants.CLOSED }) };
+  longtermClicked = () => { this.setState({ selectedStatus: statusConstants.LONGTERM }) };
 
-  openHovered = () => { this.setState({ hoveredStatus: STATUS_OPEN }) };
-  cautionHovered = () => { this.setState({ hoveredStatus: STATUS_CAUTION }) };
-  closedHovered = () => { this.setState({ hoveredStatus: STATUS_CLOSED }) };
-  longtermHovered = () => { this.setState({ hoveredStatus: STATUS_LONGTERM }) };
+  openHovered = () => { this.setState({ hoveredStatus: statusConstants.OPEN }) };
+  cautionHovered = () => { this.setState({ hoveredStatus: statusConstants.CAUTION }) };
+  closedHovered = () => { this.setState({ hoveredStatus: statusConstants.CLOSED }) };
+  longtermHovered = () => { this.setState({ hoveredStatus: statusConstants.LONGTERM }) };
 
   clearHover = () => { this.setState({ hoveredStatus: null }) };
 
@@ -44,32 +26,32 @@ class StatusToggle extends React.Component {
       <div className="StatusToggleContainer">
         <div 
           className={"StatusToggle__open" + 
-                    (this.state.selectedStatus === STATUS_OPEN ? "--selected" : "") +
-                    (this.state.hoveredStatus === STATUS_OPEN ? " hover" : "")}
+                    (this.state.selectedStatus === statusConstants.OPEN ? "--selected" : "") +
+                    (this.state.hoveredStatus === statusConstants.OPEN ? " hover" : "")}
           onClick={this.openClicked}
           onMouseOver={this.openHovered}
           onMouseLeave={this.clearHover} >
           <FontAwesome name='circle-o' />
         </div>
         <div className={"StatusToggle__caution" + 
-                       (this.state.selectedStatus === STATUS_CAUTION ? "--selected" : "") + 
-                       (this.state.hoveredStatus === STATUS_CAUTION ? " hover" : "")}
+                       (this.state.selectedStatus === statusConstants.CAUTION ? "--selected" : "") + 
+                       (this.state.hoveredStatus === statusConstants.CAUTION ? " hover" : "")}
           onClick={this.cautionClicked}
           onMouseOver={this.cautionHovered}
           onMouseLeave={this.clearHover} >
           <FontAwesome name='exclamation-triangle' />
         </div>
         <div className={"StatusToggle__closed" + 
-                       (this.state.selectedStatus === STATUS_CLOSED ? "--selected" : "") +
-                       (this.state.hoveredStatus === STATUS_CLOSED ? " hover" : "")}
+                       (this.state.selectedStatus === statusConstants.CLOSED ? "--selected" : "") +
+                       (this.state.hoveredStatus === statusConstants.CLOSED ? " hover" : "")}
           onClick={this.closedClicked}
           onMouseOver={this.closedHovered}
           onMouseLeave={this.clearHover} >
           <FontAwesome name='times' />
         </div>
         <div className={"StatusToggle__longterm" + 
-                       (this.state.selectedStatus === STATUS_LONGTERM ? "--selected" : "") +
-                       (this.state.hoveredStatus === STATUS_LONGTERM ? " hover" : "")}
+                       (this.state.selectedStatus === statusConstants.LONGTERM ? "--selected" : "") +
+                       (this.state.hoveredStatus === statusConstants.LONGTERM ? " hover" : "")}
           onClick={this.longtermClicked}
           onMouseOver={this.longtermHovered}
           onMouseLeave={this.clearHover} >
