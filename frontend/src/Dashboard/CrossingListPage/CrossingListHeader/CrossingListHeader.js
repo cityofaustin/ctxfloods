526cba63ed9848cf80d0a5804a0d77ff5115500a
Brian Smith <briaguya@gmail.com>
2017-10-15 19:52:49 -0500

    container query to adjust size

--- frontend/src/Dashboard/CrossingListPage/CrossingListHeader/CrossingListHeader.js
+++ frontend/src/Dashboard/CrossingListPage/CrossingListHeader/CrossingListHeader.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import './CrossingListHeader.css';
+import {ContainerQuery} from 'react-container-query';
+import classnames from 'classnames';
+
+const query = {
+  'fullsize': { minWidth: 768 },
+  'smallsize': { maxWidth: 767 }
+};
 
 class CrossingListHeader extends Component {
   render() {
     return (
-      <div className="CrossingListHeader">
-      	<input type="text" className="CrossingListSearch" placeholder="Search your crossings"/>
-      	<div className="CrossingListSortToggle">SORT</div>
-      	<div className="CrossingListFilter">
-      		FILTER
-      	</div>
-      </div>
+      <ContainerQuery query={query}>
+        {(params) => (
+          <div className={classnames(params, 'CrossingListHeader')}>
+            <input type="text" className={classnames(params, 'CrossingListSearch')} placeholder="Search your crossings"/>
+            <div className={classnames(params, 'CrossingListSortToggle')}>SORT</div>
+            <div className={classnames(params, 'CrossingListFilter')}>
+              FILTER
+            </div>
+          </div>
+        )}
+      </ContainerQuery>      
     );
   }
 
