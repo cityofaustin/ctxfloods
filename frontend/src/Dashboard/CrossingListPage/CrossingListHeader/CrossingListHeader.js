3278375cae226e209c6f15f57bc0ca4d317715bc
Brian Smith <briaguya@gmail.com>
2017-10-16 14:26:16 -0500

    fix search resizing

--- frontend/src/Dashboard/CrossingListPage/CrossingListHeader/CrossingListHeader.js
+++ frontend/src/Dashboard/CrossingListPage/CrossingListHeader/CrossingListHeader.js
@@ -21,7 +21,9 @@ class CrossingListHeader extends Component {
                 <FontAwesome name="search" />
               </div>
             </div>
-            <div className={classnames(params, 'CrossingListSortToggle')}>SORT</div>
+            <div className={classnames(params, 'CrossingListSortToggle')}>
+              LAST UPDATED <FontAwesome name="caret-up" />
+            </div>
             <div className={classnames(params, 'CrossingListFilter')}>
               FILTER
             </div>
