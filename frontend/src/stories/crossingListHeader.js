4ccdd71a53f8f3cce3a19600f5451cfa48df300d
Brian Smith <briaguya@gmail.com>
2017-10-17 05:50:39 -0500

    fix storybook

--- frontend/src/stories/crossingListHeader.js
+++ frontend/src/stories/crossingListHeader.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import CrossingListHeader from '../Dashboard/CrossingListPage/CrossingListHeader/CrossingListHeader';
+import schema from './schema/schema';
+import { withApolloProvider } from 'storybook-addon-apollo-graphql';
 
+const root = {
+  allCrossings: () => ({ totalCount: 6})
+};
 
 storiesOf('Crossing List Header', module)
+  .addDecorator(withApolloProvider({ schema, root }))
   .add('Full Width', () => <CrossingListHeader  />)
   .add('Width < 768px', () => 
   	<div style={{width: '400px'}}>
