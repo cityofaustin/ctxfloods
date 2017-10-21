eeb6944a994184b33d19eed144bad6b3d46b5d9f
Brian Smith <briaguya@gmail.com>
2017-10-21 18:51:06 -0500

    the polyphonic spree

--- frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
+++ frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
@@ -1,12 +1,21 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import React, { Component } from 'react';
 
 class CrossingDetails extends Component {
   render() {
+    if ( !this.props.data || this.props.data.loading ) {
+      return (<div>Loading</div>)
+    }
+
+    debugger;
+    const crossing = this.props.data.crossingById;
+
     return (
       <div>
         <div>Crossing Details: {this.props.crossingId}</div>
         <div> GPS </div>
-        <input type='text' defaultValue='name'></input>
+        <input type='text' defaultValue={crossing.name}></input>
         <div> Street Address </div>
         <input type='text' defaultValue='location notes'></input>
         <input type='text' defaultValue='community'></input>
@@ -18,4 +27,14 @@ class CrossingDetails extends Component {
 
 }
 
-export default CrossingDetails;
+const CrossingByIdQuery = gql`
+  query crossingById($crossingId:Int!) {
+    crossingById(id:$crossingId) {
+      name 
+    }
+  }
+`;
+
+export default graphql(CrossingByIdQuery, {
+  options: ({ crossingId }) => ({ variables: { crossingId } }),
+})(CrossingDetails);
