49bd6d5dd492dbf8ab760ba2a2fccc624f81011a
Brian Smith <briaguya@gmail.com>
2017-10-21 18:31:57 -0500

    the beginning stages of...

--- frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
+++ frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class CrossingDetails extends Component {
   render() {
     return (
-      <div>Crossing Details: {this.props.crossingId}</div>
+      <div>
+        <div>Crossing Details: {this.props.crossingId}</div>
+        <div> GPS </div>
+        <input type='text' defaultValue='name'></input>
+        <div> Street Address </div>
+        <input type='text' defaultValue='location notes'></input>
+        <input type='text' defaultValue='community'></input>
+        <div> SAVE CROSSING </div>
+        <div> DELETE CROSSING </div>
+      </div>
     );
   }
 
