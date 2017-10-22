bf8cddff19bcb061acbc627adbf991e578d9d975
Brian Smith <briaguya@gmail.com>
2017-10-21 20:15:00 -0500

    move crossing out to page

--- frontend/src/Dashboard/CrossingDetailPage/CrossingDetailPage.js
+++ frontend/src/Dashboard/CrossingDetailPage/CrossingDetailPage.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import CrossingDetails from './CrossingDetails';
 import CrossingStatusHistory from './CrossingStatusHistory';
 
+
 class CrossingDetailPage extends Component {
   render() {
-    const crossingId = this.props.match.params.id;
+    if ( !this.props.data || this.props.data.loading ) {
+      return (<div>Loading</div>)
+    }
+
+    const crossing = this.props.data.crossingById;
+    const community = crossing.communityCrossingsByCrossingId.nodes[0].communityByCommunityId;
 
     return (
       <div>
-        <CrossingDetails crossingId={crossingId}/>
-        <CrossingStatusHistory crossingId={crossingId}/>
+        <CrossingDetails crossing={crossing} community={community}/>
+        <CrossingStatusHistory crossingId={this.props.crossingId}/>
       </div>
     );
   }
 
 }
 
-export default CrossingDetailPage;
+const CrossingByIdQuery = gql`
+  query crossingById($crossingId:Int!) {
+    crossingById(id:$crossingId) {
+      name
+      humanCoordinates
+      humanAddress
+      description
+      communityCrossingsByCrossingId {
+        nodes {
+          communityByCommunityId {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default graphql(CrossingByIdQuery, {
+  options: (ownProps) => ({ variables: { crossingId: ownProps.match.params.id } }),
+})(CrossingDetailPage);
--- frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
+++ frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
@@ -1,26 +1,20 @@
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 import React, { Component } from 'react';
 
 class CrossingDetails extends Component {
-  render() {
-    if ( !this.props.data || this.props.data.loading ) {
-      return (<div>Loading</div>)
-    }
 
-    debugger;
-    const crossing = this.props.data.crossingById;
-    const community = crossing.communityCrossingsByCrossingId.nodes[0].communityByCommunityId;
+  render() {
+    const { crossing, community } = this.props;
 
     return (
       <div>
-        <div>Crossing Details: {this.props.crossingId}</div>
+        <div>Crossing Details: {crossing.id}</div>
         <div> {crossing.humanCoordinates} </div>
         <input type='text' defaultValue={crossing.name}></input>
         <div> {crossing.humanAddress} </div>
         <input type='text' defaultValue={crossing.description}></input>
         <input type='text' defaultValue={community.name}></input>
         <div> SAVE CROSSING </div>
+        <div> CANCEL EDITS </div>
         <div> DELETE CROSSING </div>
       </div>
     );
@@ -28,24 +22,6 @@ class CrossingDetails extends Component {
 
 }
 
-const CrossingByIdQuery = gql`
-  query crossingById($crossingId:Int!) {
-    crossingById(id:$crossingId) {
-      name
-      humanCoordinates
-      humanAddress
-      description
-      communityCrossingsByCrossingId {
-        nodes {
-          communityByCommunityId {
-            name
-          }
-        }
-      }
-    }
-  }
-`;
 
-export default graphql(CrossingByIdQuery, {
-  options: ({ crossingId }) => ({ variables: { crossingId } }),
-})(CrossingDetails);
+
+export default CrossingDetails;
