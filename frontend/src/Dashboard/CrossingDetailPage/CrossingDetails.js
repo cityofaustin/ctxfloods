0e64eb51b8f53635ae9097db79f50a7298e0f6ff
Brian Smith <briaguya@gmail.com>
2017-10-23 21:15:32 -0500

    b

--- frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
+++ frontend/src/Dashboard/CrossingDetailPage/CrossingDetails.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class CrossingDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.crossing.name,
+    };
+  }
+
+  updateCrossing(e) {
+    this.props.updateCrossingMutation({
+      variables: {
+        crossingId: this.props.crossing.id,
+        name: this.state.name
+      }
+    })
+    .then(({ data }) => {
+      alert(data);
+    }).catch((error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
+
+  nameChanged = (e) => { this.setState({ name: e.target.value }) };
 
   render() {
     const { crossing, community } = this.props;
@@ -9,7 +33,7 @@ class CrossingDetails extends Component {
       <div>
         <div>Crossing Details: {crossing.id}</div>
         <div> {crossing.humanCoordinates} </div>
-        <input type='text' defaultValue={crossing.name}></input>
+        <input type="text" value={this.state.name} onChange={this.nameChanged}/>
         <div> {crossing.humanAddress} </div>
         <input type='text' defaultValue={crossing.description}></input>
         <input type='text' defaultValue={community.name}></input>
@@ -22,6 +46,31 @@ class CrossingDetails extends Component {
 
 }
 
+const newStatusUpdateMutation = gql`
+  mutation newStatusUpdate(
+    $crossingId: Int,
+    $statusId: Int,
+    $reasonId: Int,
+    $durationId: Int,
+    $notes: String
+  ) {
+    newStatusUpdate(input: 
+      {
+        crossingId: $crossingId,
+        statusId: $statusId,
+        statusReasonId: $reasonId,
+        statusDurationId: $durationId,
+        notes: $notes
+      }) {
+      statusUpdate {
+        id
+        statusId
+        statusReasonId
+        statusDurationId
+        notes
+      }
+    }
+  }
+`;
 
-
-export default CrossingDetails;
+export default graphql(updateCrossingMutation, { name: 'updateCrossingMutation' })(CrossingDetails);
