7d85f0d8dfaa602b67985aed2524cf7c50eaa859
Simi <Simi.Damani@austintexas.gov>
2017-10-31 13:58:02 -0500

    set storybook global styles for different widths and viewport padding

--- frontend/src/stories/crossingListHeader.js
+++ frontend/src/stories/crossingListHeader.js
@@ -10,13 +10,12 @@ const root = {
 
 storiesOf('Crossing List Header', module)
   .addDecorator(withApolloProvider({ schema, root }))
-  .add('Full Width', () => <CrossingListHeader  />)
-  .add('Width 900px', () => 
-    <div style={{width: '900px'}}>
+  .add('Large Width', () => 
+    <div class="storybook--lg">
       <CrossingListHeader />
     </div>)
-  .add('Width 400px', () => 
-    <div style={{width: '400px'}}>
+  .add('Small Width', () => 
+    <div class="storybook--sm">
 	  	<CrossingListHeader />
     </div>
   );
