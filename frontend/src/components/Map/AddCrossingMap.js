f4d4fd108209b8bf630770fc6a1c7a3bb93a5167
Brian Smith <briaguya@gmail.com>
2017-12-07 01:57:59 -0600

    draggin

--- frontend/src/components/Map/AddCrossingMap.js
+++ frontend/src/components/Map/AddCrossingMap.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Marker } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import mapboxstyle from 'components/Map/mapboxstyle.json';
 import { statusIcons } from 'constants/StatusConstants';
 
-const Map = ReactMapboxGl({ accessToken: null, interactive: false });
+const Map = ReactMapboxGl({ accessToken: null });
 
 class AddCrossingMap extends Component {
 
@@ -12,12 +12,22 @@ class AddCrossingMap extends Component {
   }
 
   render() {
+    const coordinates = [-97.46, 30.96];
+
     return (
         <Map
           className="CrossingStaticMap"
           style={mapboxstyle}
+          center={coordinates}
           onStyleLoad = {this.onStyleLoad}
         >
+          <Layer
+            type="symbol"
+            id="openCrossings"
+            layout={{ 'icon-image': 'circle-15', 'icon-allow-overlap': true }}
+            >            
+             <Feature coordinates={coordinates} draggable={true}/>
+          </Layer>
         </Map>
     );
   }
