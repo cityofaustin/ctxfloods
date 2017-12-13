67367b0c7037df199aeca6eecfbd9572acd0d28a
Brian Smith <briaguya@gmail.com>
2017-12-12 19:01:09 -0600

    add status history page for all crossings

--- frontend/src/components/Dashboard/CrossingDetailPage/CrossingStatusHistory.js
+++ frontend/src/components/Dashboard/CrossingDetailPage/CrossingStatusHistory.js
@@ -22,7 +22,7 @@ class CrossingStatusHistory extends Component {
         {(params) => (
           <div className={classnames(params, "CrossingStatusHistory")}>
             <h2 className="CrossingStatusHistory__section-header">
-              Crossing History
+              Crossing Status History
             </h2>
             <div className="CrossingStatusHistory__list-wrapper">
               {history.map((update, i) => {
@@ -32,14 +32,24 @@ class CrossingStatusHistory extends Component {
                 const reason = get(update, 'statusReasonByStatusReasonId.name', 'Unconfirmed');
                 const duration = get(update, 'statusDurationByStatusDurationId.name', '--');
                 const createdAt = get(update, 'createdAt', '--');
+                const crossingId = get(update, 'crossingId', null);
+                const crossingName = get(update, 'crossingByCrossingId.name', null);
                 const notes = update.notes ? update.notes : '--';
                 const shouldDisplay = displayedInputs[statusId];
 
                 return (
                   <div className="CrossingStatusHistory__item" key={i}>
+
+                    {crossingId ? (
+                      <div className="CrossingListItemFlexItem">
+                        <a href={`/dashboard/crossing/${crossingId}`} className="CrossingName">{crossingName}</a>
+                      </div>
+                    ) : null }
+
                     <div className="flexitem">
                       <DateTime datetime={createdAt} user={user} />
                     </div>
+
                     <div className="CrossingStatusHistory__item-details-flexcontainer">
                       <div className="CrossingStatusHistory__item-details">
                         <img src={statusIcons[statusId]} alt={shouldDisplay.status} className="CrossingStatusHistory__status-icon" />
