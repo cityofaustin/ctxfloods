74473217eb6f87d64f4202ff6604b770fcdac0b7
Brian Smith <briaguya@gmail.com>
2018-01-16 05:05:55 -0600

    we will respond to logging in on the phone

--- frontend/src/components/Dashboard/Header/UserControls.js
+++ frontend/src/components/Dashboard/Header/UserControls.js
@@ -8,17 +8,26 @@ import cogSvg from 'images/cog.svg';
 import 'components/Dashboard/Header/UserControls.css'
 
 class UserControlsBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showControls: false,
+    };
+  }
+
+  toggleControls = () => { this.setState({ showControls: !this.state.showControls }) };
+
   render() {
     return (
       auth.isAuthenticated() ? (
         <div className={classnames(this.props.cqParams, "UserControls")}>
-          <img src={cogSvg} alt="Link User Settings" className="Header__settings-icon" />
-          { this.props.cqParams.fullsize && this.props.currentUser ? (
+          <img src={cogSvg} alt="Link User Settings" className="Header__settings-icon" onClick={this.toggleControls}/>
+          { (this.props.cqParams.fullsize || this.state.showControls) && this.props.currentUser ? (
             <Link className="UserControls__text" to="#">
               {`${this.props.currentUser.firstName} ${this.props.currentUser.lastName}`}
             </Link> ) : ''
           }
-          { this.props.cqParams.fullsize ? (
+          { (this.props.cqParams.fullsize || this.state.showControls) ? (
 
             <div
               className="underline UserControls__text"
