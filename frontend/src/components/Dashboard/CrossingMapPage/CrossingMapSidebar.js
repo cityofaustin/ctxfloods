0f6dc29b0fe17e4bfeb03860d13a40889035228a
Brian Smith <briaguya@gmail.com>
2018-01-29 16:00:32 -0600

    nice lookin sidebar ya got there

--- frontend/src/components/Dashboard/CrossingMapPage/CrossingMapSidebar.js
+++ frontend/src/components/Dashboard/CrossingMapPage/CrossingMapSidebar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import CrossingMapOverlay from 'components/Dashboard/CrossingMapPage/CrossingMapOverlay';
 import 'components/Dashboard/CrossingMapPage/CrossingMapPage.css';
+import FontAwesome from 'react-fontawesome';
 
 class CrossingMapSidebar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      visible: false,
+      visible: true,
     };
   }
 
@@ -22,7 +23,12 @@ class CrossingMapSidebar extends Component {
       <div className="CrossingMapPage_sidebar-container">{visible && (
         <div className="CrossingMapPage_sidebar-content"> SIDEBAR </div>
         )}
-        <div className="CrossingMapPage_sidebar-toggle" onClick={this.toggleSidebar}> SHOW HIDE TOGGLE </div>
+        <div className="CrossingMapPage_sidebar-toggle" onClick={this.toggleSidebar}>
+          { visible ?
+            <FontAwesome name='angle-left' size='2x' onClick={this.toggleSidebar}/> : 
+            <FontAwesome name='angle-right' size='2x' onClick={this.toggleSidebar}/>
+          }
+        </div>
       </div>
     );
   }
