1621c83cff114e3a6cdf27cd5bf82c08f5126913
Brian Smith <briaguya@gmail.com>
2018-01-29 19:12:54 -0600

    we've got ourselves a search box

--- frontend/src/components/Dashboard/CrossingMapPage/CrossingMapSidebar.js
+++ frontend/src/components/Dashboard/CrossingMapPage/CrossingMapSidebar.js
@@ -27,7 +27,18 @@ class CrossingMapSidebar extends Component {
             SEARCH FOR A PLACE, AREA, OR CROSSING
           </div>
           <div className="CrossingMapPage_sidebar-search-container">
-            TODO SEARCH BOX HERE
+            <div className="CrossingMapPage_sidebar-search-location-icon">
+              <FontAwesome name='map-marker' size='2x'/>  
+            </div>
+            <div className="CrossingMapPage_sidebar-search-text-entry">
+              <input type="text" placeholder="Search..."/>
+            </div>
+            <div className="CrossingMapPage_sidebar-search-glass-icon">
+              <FontAwesome name='search' size='2x'/>  
+            </div>
+            <div className="CrossingMapPage_sidebar-search-cancel-icon">
+              <FontAwesome name='times' size='2x'/>  
+            </div>
           </div>
           {selectedCrossingId && <SelectedCrossingContainer crossingId={selectedCrossingId} currentUser={currentUser} selectCrossing={selectCrossing}/>}
         </div>
