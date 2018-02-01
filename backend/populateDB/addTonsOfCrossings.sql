c3b24dc3b4d7dcc5cc55915b121ea054eb504524
Brian Smith <briaguya@gmail.com>
2018-02-01 03:30:53 -0600

    update local server with tons of crossings script to not have dummy crossing data

--- backend/populateDB/addTonsOfCrossings.sql
+++ backend/populateDB/addTonsOfCrossings.sql
@@ -1,5 +1,38 @@
 begin;
 
+-- Add statuses
+insert into floods.status (id, name) values
+  (1, 'Open'),
+  (2, 'Closed'),
+  (3, 'Caution'),
+  (4, 'Long-Term Closure');
+alter sequence floods.status_id_seq restart with 5;
+
+-- Add status reasons
+insert into floods.status_reason (id, status_id, name) values
+  (1, 2, 'Flooded'),
+  (2, 4, 'Bridge Broken'),
+  (3, 3, 'Unconfirmed Flooding');
+alter sequence floods.status_reason_id_seq restart with 4;
+
+-- Add status durations
+insert into floods.status_duration (id, name, timespan) values
+  (1, 'A Minute', interval '1 minute'),
+  (2, 'A Week', interval '1 week');
+alter sequence floods.status_duration_id_seq restart with 3;
+
+-- Add status associations
+insert into floods.status_association (id, status_id, detail, rule) values
+  (1, 1, 'reason', 'disabled'),
+  (2, 1, 'duration', 'disabled'),
+  (3, 2, 'reason', 'required'),
+  (4, 2, 'duration', 'disabled'),
+  (5, 3, 'reason', 'required'),
+  (6, 3, 'duration', 'disabled'),
+  (7, 4, 'reason', 'required'),
+  (8, 4, 'duration', 'required');
+alter sequence floods.status_association_id_seq restart with 9;
+
 -- Add crossings
 insert into floods.crossing (community_ids, id, name, human_address, description, coordinates, geojson) values
 
@@ -1971,6 +2004,8 @@ insert into floods.community (id, name, viewportgeojson) values (9018, 'FCO', ST
 -- Make sure they're local so we actually use the token outside of this script
 select set_config('jwt.claims.community_id', '1', true);
 select set_config('jwt.claims.role', 'floods_super_admin', true);
+-- Add super admin
+select floods.register_user(text 'Super', text 'Admin', text 'Superhero, Administrator', integer '9009', text '867-5309', text 'superadmin@flo.ods', text 'texasfloods', text 'floods_super_admin');
 -- Add community admins to each new community
 select floods.register_user(text 'LEA', text 'Admin', text 'Community Administrator', integer '9001', text '867-5309', text 'admin@lea.floods', text 'texasfloods', text 'floods_community_admin');
 select floods.register_user(text 'ALL', text 'Admin', text 'Community Administrator', integer '9002', text '867-5309', text 'admin@all.floods', text 'texasfloods', text 'floods_community_admin');
