beacab8c05e4186f99b6dd33198740ef2be5e3f2
Luqmaan <ldawoodjee@gmail.com>
2018-04-16 10:18:50 -0500

    Fix proptypes

--- src/components/Public/PublicHeader.js
+++ src/components/Public/PublicHeader.js
@@ -14,7 +14,7 @@ export default function PublicHeader({ location }) {
           <a href="/">Translate</a>
         </div>
       </TopBar>
-      <Header>
+      <Header location={location}>
         <li
           className={
             location.pathname.includes('/map')
@@ -59,5 +59,4 @@ export default function PublicHeader({ location }) {
 
 PublicHeader.propTypes = {
   location: PropTypes.object.isRequired,
-  currentUser: PropTypes.object.isRequired,
 };
