92f0ecfdc15591fb62008a7b49846e8b1d0e257a
Niicck <niicck@users.noreply.github.com>
2018-10-29 21:21:35 -0500

    remove jwt on error

--- src/services/apolloClientService.js
+++ src/services/apolloClientService.js
@@ -1,12 +1,24 @@
 import ApolloClient from 'apollo-client';
-import { ApolloLink, concat } from 'apollo-link';
+import { ApolloLink, from } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from "apollo-link-error";
+import { logError } from './logger';
 
 import { isTokenExpired } from './jwtHelper';
 
 const httpLink = createHttpLink({
   uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+  if ( graphQLErrors.name === "JsonWebTokenError") {
+    localStorage.removeItem('jwt_user_token');
+    return forward(operation);
+  }
+  // TODO: make an error page for displaying all errors
+  logError(graphQLErrors);
+  logError(networkError);
 });
 
 const jwtMiddleware = new ApolloLink((operation, forward) => {
@@ -24,7 +36,7 @@ const jwtMiddleware = new ApolloLink((operation, forward) => {
 });
 
 const client = new ApolloClient({
-  link: concat(jwtMiddleware, httpLink),
+  link: from([jwtMiddleware, errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
